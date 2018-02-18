e562deea5aed1ca850e9e37d381f1321e4ad4890
Gideon Richter <gideonrich@hotmail.ca>
2018-02-17 21:25:21 -0700

    Change assignment_1 to QTSpim

--- assignment_1.asm
+++ assignment_1.asm
@@ -3,38 +3,6 @@
 # Input an array, find min and max values, then output 
 
 .data
-	
-	.macro print_str(%str)
-  # :param %str addr: Address of .asciiz 
-  li $v0, 4 					
-  la $a0, %str 			
-  syscall
-.end_macro
-
-.macro print_int(%int)
-  # :param %int addr: Register/Address of word
-  li $v0, 1						
-  la $a0, (%int)
-  syscall 
-.end_macro
-
-.macro get_integer_from_user()
-  # :side_effect: input stored in $v0
-  li $v0, 5					 
-  syscall
-.end_macro
-	
-.macro end_program()
-  li $v0, 10						
-  syscall
-.end_macro
-
-.macro increase_by(%x, %amount)
-  # :param %x reg: Register with numeric value
-  # :param %amount: Amount to increase %x by
-  addi %x, %x, %amount
-.end_macro 
-
   prompt_input_size: .asciiz "Please enter the size of the list: " 
   prompt_input_value: .asciiz "Please enter the element\n"
 	
@@ -52,15 +20,20 @@ main:
   #
   # Get user to input list
   # 
-  print_str(prompt_input_size) 
-  get_integer_from_user()
+  li $v0, 4 # Print string
+  la $a0, prompt_input_size
+  syscall
+  
+  li $v0, 5 # Get int list size from user
+  syscall
+
 		
   sw $v0, list_length # Store list length in memory
-  lw $a1, list_length	# Load list length into $a1
+  lw $a1, list_length # Load list length into $a1
 	
-  li $s1, 0 		# Initialize loop counter to 0
-  li $t2, 0 		# Initialize offset to 0
-  la $t0, list 	# Load address of list to $t0
+  li $s1, 0 # Initialize loop counter to 0
+  li $t2, 0 # Initialize offset to 0
+  la $t0, list # Load address of list to $t0
 	
   #
   # Fill list and find min and max at same time 
@@ -68,29 +41,33 @@ main:
   li $s5, 1 # Boolean flag, first iteration
 	
   input_loop: 
-    increase_by($s1, 1) 					# Increment index
-    bgt  $s1, $a1, end_input_loop	# If index > size: Break
+    add $s1, $s1, 1 # Increment index
+    bgt  $s1, $a1, end_input_loop # If index > size: Break
 	
-    print_str(prompt_input_value)
-    get_integer_from_user()
+    li $v0, 4 # Print string
+    la $a0, prompt_input_value
+    syscall
+    
+    li $v0, 5 # Get int list value from user
+    syscall
 		
-    beqz $s5, continue_first_time	# If not first time, skip
-		move $s3, $v0									# Min = Input 
-		move $s4, $v0									# Max = Input
-		li   $s5, 0										# Skip next time
+    beqz $s5, continue_first_time # If not first time, skip
+		move $s3, $v0 # Min = Input 
+		move $s4, $v0 # Max = Input
+		li   $s5, 0	# Skip next time
 		continue_first_time:
 		
-		bgt $v0, $s3, continue_min 		# If input > min: Skip next line
-		move $s3, $v0								 	# Min = Input			
+		bgt $v0, $s3, continue_min # If input > min: Skip next line
+		move $s3, $v0 # Min = Input			
 		continue_min:  
 		
-		blt $v0, $s4, continue_max 		# If input < max: Skip next line			
-		move $s4, $v0 								# Max = Input 
+		blt $v0, $s4, continue_max # If input < max: Skip next line			
+		move $s4, $v0 # Max = Input 
 		continue_max:
 
-		add  $t3, $t0, $t2 						# Calculate address to store value in list
-		sw   $v0, 0($t3) 							# Store value in list address 
-		increase_by($t2, 4) 					# Offset = Next element
+		add  $t3, $t0, $t2 # Calculate address to store value in list
+		sw   $v0, 0($t3) # Store value in list address 
+		addi $t2, $t2, 4
 			
 		j input_loop						
 	end_input_loop: 	
@@ -98,32 +75,49 @@ main:
 	#
 	# Output Sorted List to Screen 
 	#
-	li $s1, 0 						# Init index to 0 
-	li $t2, 0 						# Init offset to 0
-	lw $a1, list_length 	# Load list length to $a1
-	la $t0, list 					# Load list address to $t0
+	li $s1, 0 # Init index to 0 
+	li $t2, 0 # Init offset to 0
+	lw $a1, list_length # Load list length to $a1
+	la $t0, list # Load list address to $t0
 	
-	print_str(the_list_is_string)
+	li $v0, 4 # Print string
+	la $a0, the_list_is_string
+	syscall
 		
 	output_loop: 
-		increase_by($s1, 1) 						# Index = Index + 1
-		bgt  $s1, $a1, end_output_loop	# If index > size: Break
+		addi $s1, $s1, 1 # Index = Index + 1
+		bgt  $s1, $a1, end_output_loop # If index > size: Break
 			
-		add $t3, $t0, $t2 	# Current element = List address + Offset
-		lw  $a0, 0($t3) 		# Load value of current element to $a0
+		add $t3, $t0, $t2 # Current element = List address + Offset
+		lw  $a0, 0($t3) # Load value of current element to $a0
 			
-		print_int($a0)				
-		print_str(space)				
+		li $v0, 1 # Print integer
+		syscall	
+		
+		li $v0, 4 # Print string
+		la $a0, space
+		syscall					
 			
-		increase_by($t2, 4) # Offset = Next element 
-			
+		addi $t2, $t2, 4 # Offset = Next element 
+				
 		j output_loop		
 	end_output_loop: 
 	
-	print_str(the_min_is_string)
-	print_int($s3)
+	li $v0, 4 # Print string
+	la $a0, the_min_is_string
+	syscall
 	
-	print_str(the_max_is_string)
-	print_int($s4)
+	li $v0, 1 # Print integer (min)
+	move $a0, $s3
+	syscall
+	
+	li $v0, 4 # Print string 
+	la $a0, the_max_is_string
+	syscall
+	
+	li $v0, 1 # Print integer (max)
+	move $a0, $s4
+	syscall
 			
-	end_program()
+	li $v0, 10 # End program
+	syscall 
