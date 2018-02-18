4b36600d793b75dc0fa2dce4122d79af4349ce24
Gideon Richter <gideonrich@hotmail.ca>
2018-02-17 21:32:32 -0700

    Removed macros from assignment_2.asm

--- assignment_2.asm
+++ assignment_2.asm
@@ -11,74 +11,82 @@
   	mul_result_str: .asciiz "\nThe multiplication result is: " 
   	div_result_str: .asciiz "\nThe division result is: "
 	
-.text
-
-.macro print_str(%str)
- 	 # :param %str addr: Address of .asciiz 
-  	li $v0, 4 					
-  	la $a0, %str 			
-  	syscall
-.end_macro
-
-.macro print_int(%int)
-  	# :param %int addr: Register/Address of word
-  	li $v0, 1						
-  	la $a0, (%int)
-  	syscall 
-.end_macro
-
-.macro get_int_from_user()
-  	# :side_effect: input stored in $v0
-  	li $v0, 5					 
-  	syscall
-.end_macro
-	
-.macro end_program()
-  	li $v0, 10						
-  	syscall
-.end_macro
-
 .text 
 
 main: 
 	
-	print_str(prompt_input_x)
-	get_int_from_user()
+	li $v0, 4 # Print string
+	la $a0, prompt_input_x
+	syscall
+	
+	li $v0, 5 # Get int x from user
+	syscall
+	
 	move $s1, $v0
 	
-	print_str(prompt_input_y)
-	get_int_from_user()
+	li $v0, 4 # Print string
+	la $a0, prompt_input_y
+	syscall
+	
+	li $v0, 5 # Get int y from user
+	syscall
+	
 	move $s2, $v0
 	
 	#
 	# Addition
 	#
 	add $t0, $s1, $s2
-	print_str(add_result_str)
-	print_int($t0)
+	
+	li $v0, 4 # Print string
+	la $a0, add_result_str
+	syscall
+	
+	li $v0, 1 # Print integer result of x + y
+	move $a0, $t0
+	syscall
 	
 	#
 	# Subtraction 
 	#
 	sub $t0, $s1, $s2
-	print_str(sub_result_str)
-	print_int($t0)
+	
+	li $v0, 4 # Print string
+	la $a0, sub_result_str
+	syscall
+	
+	li $v0, 1 # Print integer result of x - y
+	move $a0, $t0
+	syscall
 	
 	#
 	# Multiplication 
 	#
 	mul $t0, $s1, $s2
-	print_str(mul_result_str)
-	print_int($t0)
+	
+	li $v0, 4 # Print string
+	la $a0, mul_result_str
+	syscall
+	
+	li $v0, 1 # Print integer result of x * y
+	move $a0, $t0
+	syscall
 	
 	#
 	# Division
 	#
 	div $t0, $s1, $s2 
-	print_str(div_result_str)
-	print_int($t0)
 	
-	end_program()
+	li $v0, 4 # Print string
+	la $a0, div_result_str
+	syscall
+	
+	li $v0, 1 # Print integer result of x / y
+	move $a0, $t0
+	syscall
+	
+	li $v0, 10 # End program 
+	syscall
 	
 
 	
