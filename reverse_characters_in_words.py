d01b493b73354fe91b93755fff06643f3797e6f6
Gideon <gideonrich@hotmail.ca>
2018-02-26 21:13:13 -0700

    Add comments, and gitignore

--- reverse_characters_in_words.py
+++ reverse_characters_in_words.py
@@ -1,12 +1,13 @@
-class WordReverser(object):
-    """Reverses the characters within each word of a string while maintaining
-    the original word order. Written in an unpythonic way because I was
-    porting it to MIPS Assembly Language"""
+class ReverseWordCharactersInString(object):
+    """Reverses characters within each word of string while maintaining original
+    word order. Written in an unpythonic way to port to MIPS Assembly Language"""
     def __init__(self, string):
         self.string = string
         self.reversed_string = []
 
     def reverse_string(self):
+        """Iterate through string - when space is found, reverse previous word and
+        append to reversed_string. Otherwise, add character to word"""
         i = 0
         word = []
         n = len(self.string)
@@ -14,15 +15,18 @@ class WordReverser(object):
         while i < n:
             char = self.string[i]
             if char == " ":
+                # At end of a word or repeated space
                 self._reverse_word(word)
                 self.reversed_string.append(" ")
             else:
                 word.append(char)
             i += 1
 
+        """Reversing here decreases loop logic to handle special end case"""
         self._reverse_word(word)
 
     def _reverse_word(self, word):
+        """Pop last character and append to end of reversed string"""
         i = 0
         n = len(word)
 
@@ -32,6 +36,6 @@ class WordReverser(object):
             i += 1
 
 if __name__ == "__main__":
-    x = WordReverser("hello world nice")
-    x.reverse_string()
+    x = ReverseWordCharactersInString("hello world nice")
+    x.reverse_string()  # This is janky but oh well (should be automatic)
     print x.reversed_string
--- test_reverse_characters_in_words.py
+++ test_reverse_characters_in_words.py
@@ -1,26 +1,26 @@
 from unittest import TestCase
 
-from reverse_characters_in_words import WordReverser
+from reverse_characters_in_words import ReverseWordCharactersInString
 
-class TestWordReverser(TestCase):
+class TestReverseWordCharactersInString(TestCase):
     def run_test(self, Reverser):
         Reverser.reverse_string()
         return Reverser.reversed_string
 
     def test_empty_string(self):
-        x = WordReverser("")
+        x = ReverseWordCharactersInString("")
         expected_result = []
         result = self.run_test(x)
         self.assertEqual(result, expected_result)
 
     def test_all_spaces(self):
-        x = WordReverser("   ")
+        x = ReverseWordCharactersInString("   ")
         expected_result = [" ", " ", " "]
         result = self.run_test(x)
         self.assertEqual(result, expected_result)
 
     def test_normal_string(self):
-        x = WordReverser("hello world nice")
+        x = ReverseWordCharactersInString("hello world nice")
         expected_result = ["o", "l", "l", "e", "h", " ", "d", "l", "r", "o", "w", " ", "e", "c", "i", "n"]
         result = self.run_test(x)
         self.assertEqual(result, expected_result)
